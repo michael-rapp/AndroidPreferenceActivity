57507a3e8cf20e1b40b516ef523d3ae3111499f6
mrapp <mrapp@localhost>
2014-10-07 23:43:04 +0200

    Added the interface AdapterListener.

---
+++ src/de/mrapp/android/preference/adapter/AdapterListener.java
@@ -0,0 +1,52 @@
+/*
+ * AndroidPreferenceActivity Copyright 2014 Michael Rapp
+ *
+ * This program is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU Lesser General Public License as published 
+ * by the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>. 
+ */
+package de.mrapp.android.preference.adapter;
+
+import de.mrapp.android.preference.PreferenceHeader;
+
+/**
+ * Defines the interface, a class, which should be notified, when the underlying
+ * data of a {@link PreferenceHeaderAdapter} has been changed, must implement.
+ * 
+ * @author Michael Rapp
+ *
+ * @since 1.0.0
+ */
+public interface AdapterListener {
+
+	/**
+	 * The method, which is invoked, when a preference header has been added to
+	 * the adapter.
+	 * 
+	 * @param preferenceHeader
+	 *            The preference header, which has been added to the adapter, as
+	 *            an instance of the class {@link PreferenceHeader}
+	 */
+	void onPreferenceHeaderAdded(PreferenceHeader preferenceHeader);
+
+	/**
+	 * The method, which is invoked, when a preference header has been removed
+	 * from the adapter.
+	 * 
+	 * @param preferenceHeader
+	 *            The preference header, which has been removed from the
+	 *            adapter, as an instance of the class {@link PreferenceHeader}
+	 */
+	void onPreferenceHeaderRemoved(PreferenceHeader preferenceHeader);
+
+}
