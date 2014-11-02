2323c59b92f1f44b7003bf530f2cc7d073bc1abb
Michael Rapp <michael.rapp90@googlemail.com>
2014-11-02 19:09:43 +0100

    Added ensureAtMaximum-method.

--- src/de/mrapp/android/preference/activity/util/Condition.java
+++ src/de/mrapp/android/preference/activity/util/Condition.java
@@ -118,4 +118,26 @@ public final class Condition {
 		}
 	}
 
+	/**
+	 * Ensures, that a {@link Float} value is at maximum a specific value.
+	 * Otherwise an {@link IllegalArgumentException} with a specific message
+	 * will be thrown.
+	 * 
+	 * @param value
+	 *            The value, which should be checked, as a {@link Float} value
+	 * @param referenceValue
+	 *            The value, the given value must be at maximum, as a
+	 *            {@link Float} value
+	 * @param exceptionMessage
+	 *            The message of the {@link IllegalArgumentException}, which is
+	 *            thrown, if the given value is greater than the reference
+	 *            value, as a {@link String}
+	 */
+	public static void ensureAtMaximum(final float value,
+			final float referenceValue, final String exceptionMessage) {
+		if (value > referenceValue) {
+			throw new IllegalArgumentException(exceptionMessage);
+		}
+	}
+
 }
