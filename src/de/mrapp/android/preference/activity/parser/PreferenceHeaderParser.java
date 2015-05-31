a10680865dbfaf68b03635565e06846debc85622
Michael Rapp <michael.rapp90@googlemail.com>
2015-06-01 00:52:15 +0200

    Updated copyright information.

--- src/de/mrapp/android/preference/activity/parser/PreferenceHeaderParser.java
+++ src/de/mrapp/android/preference/activity/parser/PreferenceHeaderParser.java
@@ -1,5 +1,5 @@
 /*
- * AndroidPreferenceActivity Copyright 2014 Michael Rapp
+ * AndroidPreferenceActivity Copyright 2014 - 2015 Michael Rapp
  *
  * This program is free software: you can redistribute it and/or modify 
  * it under the terms of the GNU Lesser General Public License as published 
